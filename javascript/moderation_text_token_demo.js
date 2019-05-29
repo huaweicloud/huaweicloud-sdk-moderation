cff8f8ba3ef29dc98e78acd4a7ac1664288605be
Meekol <942049649@qq.com>
2019-05-29 09:43:34 +0800

    JavaScript版本的内容审核服务sdk迁移

---
+++ javascript/moderation_text_token_demo.js
@@ -0,0 +1,24 @@
+/**
+ *  图像内容检测服务token请求方式的使用示例
+ */
+var text = require("./moderation_sdk/moderation_text");
+var token = require("./moderation_sdk/gettoken");
+var utils = require("./moderation_sdk/utils");
+
+// 初始化服务的区域信息，目前支持华北-北京一(cn-north-1)、亚太-香港(ap-southeast-1)等区域信息
+utils.initRegion("cn-north-1");
+
+var username = "*******";        // 配置用户名
+var domain_name = "*******";     // 配置用户名
+var password = "*******";        // 密码
+
+token.getToken(username, domain_name, password, function (token) {
+
+    text.moderation_text(token, [{
+            "text": "666666luo聊请+110亚砷酸钾六位qq，fuck666666666666666",
+            "type": "content"}],
+        ["ad", "abuse", "politics", "porn", "contraband"], function (result) {
+            console.log(result);
+        })
+
+});
