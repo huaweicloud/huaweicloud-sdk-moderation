0abc42c63502f0ecb6b029e64a99e45fa5542749
Meekol <942049649@qq.com>
2019-05-30 14:55:49 +0800

    c#版本的内容审核服务sdk迁移

---
+++ c#/demo/ModerationServiceSample.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using Moderation.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ModerationDemo
+{
+    class ModerationServiceSample
+    {
+        static void Main(string[] args)
+        {
+            // Services currently support North China-Beijing 1 (cn-north-1), Asia Pacific-Hong Kong (ap-southeast-1)
+            String regionName = "*******";
+            String username = "*******";
+            String password = "*******";
+            String domainName = "*******";
+
+            // domain names for moderation service
+            String MODERATION_ENDPOINT = ModerationService.getEndponit(regionName);
+
+            // get token domain name 
+            String IAM_ENPOINT = "iam.cn-north-1.myhuaweicloud.com";
+
+            String token = Authentication.GetToken(username, domainName, password, regionName, IAM_ENPOINT);
+
+            // distortion correction service example
+            DistortionCorrect(token, MODERATION_ENDPOINT);
+
+            // clarity detect service example
+            ClarityDetect(token, MODERATION_ENDPOINT);
+
+            // image anti porn service example
+            AntiPorn(token, MODERATION_ENDPOINT);
+
+            // image content detect service example
+            ImageContent(token, MODERATION_ENDPOINT);
+
+            // moderation text detect service example
+            ModerationText(token, MODERATION_ENDPOINT);
+
+            // moderation video service example
+            ModerationVideo(token, MODERATION_ENDPOINT);
+
+            // image content batch jobs service example
+            ImageContentBatchJobs(token, MODERATION_ENDPOINT);
+
+            // image content batch service example
+            ImageContentBatch(token, MODERATION_ENDPOINT);
+
+        }
+
+        private static void ClarityDetect(String token, String endpoint)
+        {
+            // The obs url of file
+            String dataUrl = "";
+            // The clarity confidence interval,default 0.8f
+            float threshold = 0.8f;   
+
+            // post data by native file
+            String data = utils.ConvertFileToBase64("../../data/moderation-clarity-detect.jpg");
+            String reslut = ModerationService.ClarityDetectToken(token, data, dataUrl, threshold, endpoint);
+            Console.WriteLine(reslut);
+
+            // The OBS link must match the region, and the OBS resources of different regions are not shared
+            dataUrl = "https://ais-sample-data.obs.cn-north-1.myhuaweicloud.com/vat-invoice.jpg";
+
+            // post data by obs url
+            reslut = ModerationService.ClarityDetectToken(token, "", dataUrl, threshold, endpoint);
+            Console.WriteLine(reslut);
+            Console.ReadKey();
+
+        }
+
+        private static void DistortionCorrect(String token, String endpoint)
+        {
+            // The obs url of file
+            String dataUrl = "";
+            // Whether to correct distortion or not
+            bool correction = false;   
+
+            // post data by native file
+            String data = utils.ConvertFileToBase64("../../data/modeation-distortion.jpg");
+            String reslut = ModerationService.DistortionCorrectToken(token, data, dataUrl, correction, endpoint);
+            JObject joResult = (JObject)JsonConvert.DeserializeObject(reslut);
+            if (joResult["result"]["data"].ToString() != "")
+            {
+                String resultPath = @"../../data/modeation-distortion-token-1.bmp";
+                resultPath = utils.Base64ToFileAndSave(joResult["result"]["data"].ToString(), resultPath);
+                Console.WriteLine(resultPath);
+            }
+            else
+            {
+                Console.WriteLine(reslut);
+            }
+
+            // The OBS link must match the region, and the OBS resources of different regions are not shared
+            dataUrl = "https://ais-sample-data.obs.cn-north-1.myhuaweicloud.com/vat-invoice.jpg";
+
+            // post data by obs url
+            reslut = ModerationService.DistortionCorrectToken(token, "", dataUrl, correction, endpoint);
+            joResult = (JObject)JsonConvert.DeserializeObject(reslut);
+            if (joResult["result"]["data"].ToString() != "")
+            {
+                String resultPath = @"../../data/modeation-distortion-token-2.bmp";
+                resultPath = utils.Base64ToFileAndSave(joResult["result"]["data"].ToString(), resultPath);
+                Console.WriteLine(resultPath);
+            }
+            else
+            {
+                Console.WriteLine(reslut);
+            }
+            Console.ReadKey();
+
+        }
+
+        private static void AntiPorn(String token, String endpoint)
+        {
+            // The obs url of file
+            String dataUrl = "";    
+
+            // post data by native file
+            String data = utils.ConvertFileToBase64("../../data/moderation-antiporn.jpg");
+            String reslut = ModerationService.AntiPornToken(token, data, dataUrl, endpoint);
+            Console.WriteLine(reslut);
+
+            // The OBS link must match the region, and the OBS resources of different regions are not shared
+            dataUrl = "https://ais-sample-data.obs.cn-north-1.myhuaweicloud.com/antiporn.jpg";
+
+            // post data by obs url
+            reslut = ModerationService.AntiPornToken(token, "", dataUrl, endpoint);
+            Console.WriteLine(reslut);
+            Console.ReadKey();
+        }
+
+        private static void ImageContent(String token, String endpoint)
+        {
+            // The obs url of file 
+            String dataUrl = "";
+            // The image content confidence interval,"politics" default 0.48f,"terrorism":0
+            float threshold = 0.6f; 
+
+            JArray categories = new JArray();                                           
+
+            categories.Add("politics");
+            categories.Add("terrorism");
+            categories.Add("porn");
+
+            // post data by native file
+            String data = utils.ConvertFileToBase64("../../data/moderation-terrorism.jpg");
+            String reslut = ModerationService.ImageContentToken(token, data, dataUrl, threshold, categories, endpoint);
+            Console.WriteLine(reslut);
+
+            // The OBS link must match the region, and the OBS resources of different regions are not shared
+            dataUrl = "https://ais-sample-data.obs.cn-north-1.myhuaweicloud.com/terrorism.jpg";
+
+            // post data by obs url
+            reslut = ModerationService.ImageContentToken(token, "", dataUrl, threshold, categories, endpoint);
+            Console.WriteLine(reslut);
+            Console.ReadKey();
+        }
+
+        private static void ImageContentBatch(String token, String endpoint)
+        {
+            // The OBS link must match the region, and the OBS resources of different regions are not shared
+            String dataUrl1 = "https://ais-sample-data.obs.cn-north-1.myhuaweicloud.com/terrorism.jpg";
+            String dataUrl2 = "https://ais-sample-data.obs.cn-north-1.myhuaweicloud.com/antiporn.jpg";
+
+            JArray urls = new JArray();
+            urls.Add(dataUrl1);
+            urls.Add(dataUrl2);
+
+            float threshold = 0.6f;
+
+            JArray categories = new JArray();
+            categories.Add("politics");
+            categories.Add("terrorism");
+            categories.Add("porn");
+
+            String reslut = ModerationService.ImageContentBatchToken(token, urls, threshold, categories, endpoint);
+            Console.WriteLine(reslut);
+            Console.ReadKey();
+        }
+
+        private static void ImageContentBatchJobs(String token, String endpoint)
+        {
+
+            // The OBS link must match the region, and the OBS resources of different regions are not shared
+            String dataUrl1 = "https://ais-sample-data.obs.cn-north-1.myhuaweicloud.com/terrorism.jpg";
+            String dataUrl2 = "https://ais-sample-data.obs.cn-north-1.myhuaweicloud.com/antiporn.jpg";
+
+            JArray urls = new JArray();
+            urls.Add(dataUrl1);
+            urls.Add(dataUrl2);
+
+            JArray categories = new JArray();
+            categories.Add("politics");
+            categories.Add("terrorism");
+            categories.Add("porn");
+
+            String reslut = ModerationService.ImageContentBatchJobsToken(token, urls, categories, endpoint);
+            Console.WriteLine(reslut);
+            Console.ReadKey();
+        }
+
+        private static void ModerationText(String token, String endpoint)
+        {
+            JArray categories = new JArray();
+            categories.Add("politics");
+            categories.Add("porn");
+            categories.Add("contraband");
+            categories.Add("ad");
+
+            JArray items = new JArray();
+            JObject content = new JObject();
+            content.Add("text", "666666luo聊请+110亚砷酸钾六位qq，fuck666666666666666");
+            content.Add("type", "content");
+            items.Add(content);
+
+            String reslut = ModerationService.ModerationTextToken(token, categories, items, endpoint);
+            Console.WriteLine(reslut);
+            Console.ReadKey();
+        }
+
+        private static void ModerationVideo(String token, String endpoint)
+        {
+
+            JArray categories = new JArray();
+            categories.Add("terrorism");
+            categories.Add("porn");
+            categories.Add("politics");
+
+            // The OBS link must match the region, and the OBS resources of different regions are not shared
+            String url = "https://obs-test-llg.obs.cn-north-1.myhuaweicloud.com/bgm_recognition";
+            // Frame time interval
+            int frame_interval = 5;             
+
+            String reslut = ModerationService.VideoToken(token, url, frame_interval, categories, endpoint);
+            Console.WriteLine(reslut);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c#/src/Models/ModerationService.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Moderation.Models
+{
+    public class ModerationService
+    {
+        private static Dictionary<String, String> endPointsDic = new Dictionary<string, string>();
+
+        static ModerationService()
+        {
+            endPointsDic.Add("cn-north-1", "moderation.cn-north-1.myhuaweicloud.com");
+            endPointsDic.Add("ap-southeast-1", "moderation.ap-southeast-1.myhuaweicloud.com");
+        }
+
+        public static String ClarityDetectToken(String token, String image, String url, float threshold, String endpoint)
+        {
+            // reuqest data for image clarity detect
+            JObject requestBody = new JObject();
+            requestBody.Add("image", image);
+            requestBody.Add("url", url);
+            requestBody.Add("threshold", threshold);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.IMAGE_CLARITY_DETECT).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+        public static String DistortionCorrectToken(String token, String image, String url, bool correction, String endpoint)
+        {
+            // reuqest data for distortion correct
+            JObject requestBody = new JObject();
+            requestBody.Add("image", image);
+            requestBody.Add("url", url);
+            requestBody.Add("correction", correction);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.DISTORTION_CORRECTION).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+        public static String AntiPornToken(String token, String image, String url, String endpoint)
+        {
+            // reuqest data for image anti porn
+            JObject requestBody = new JObject();
+            requestBody.Add("image", image);
+            requestBody.Add("url", url);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.IMAGE_ANTI_PORN).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+        public static String ImageContentToken(String token, String image, String url, float threshold, JArray categories, String endpoint)
+        {
+            // reuqest data for image content detect
+            JObject requestBody = new JObject();
+            requestBody.Add("image", image);
+            requestBody.Add("url", url);
+            requestBody.Add("categories", categories);
+            requestBody.Add("threshold", threshold);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.IMAGE_CONTENT).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+        public static String ImageContentBatchToken(String token, JArray urls, float threshold, JArray categories, String endpoint)
+        {
+            // reuqest data for image content detect
+            JObject requestBody = new JObject();
+            requestBody.Add("urls", urls);
+            requestBody.Add("categories", categories);
+            requestBody.Add("threshold", threshold);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.IMAGE_CONTENT_BATCH).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+        public static String ModerationTextToken(String token, JArray categories, JArray items, String endpoint)
+        {
+            // reuqest data for moderation text
+            JObject requestBody = new JObject();
+            requestBody.Add("categories", categories);
+            requestBody.Add("items", items);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.MODERATION_TEXT).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            return utils.PostData(request, uri, token, requestBody, result, serviceName);
+
+        }
+
+
+        public static String VideoToken(String token, String url, int frame_interval, JArray categories, String endpoint)
+        {
+            String jobId = GetJobId(token, url, frame_interval, categories, endpoint);
+
+            return getResult(endpoint, jobId, token);
+
+        }
+
+        private static String GetJobId(String token, String url, int frame_interval, JArray categories, String endpoint)
+        {
+            // reuqest data for moderation video
+            JObject requestBody = new JObject();
+            requestBody.Add("categories", categories);
+            requestBody.Add("frame_interval", frame_interval);
+            requestBody.Add("url", url);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.MODERATION_VIDEO).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+            result = utils.PostData(request, uri, token, requestBody, result, serviceName);
+            JObject joResult = (JObject)JsonConvert.DeserializeObject(result);
+
+            return joResult["result"]["job_id"].ToString();
+        }
+
+        private static String getResult(String endpoint, String jobId, String token)
+        {
+            String result = null;
+            String url = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.MODERATION_VIDEO).Append("?job_id=").Append(jobId).ToString();
+
+            HttpWebRequest request = null;
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+            int retryTimes = 0;
+
+            while (true)
+            {
+                HttpWebResponse response = utils.getData(request, url, token, result, serviceName);
+                int httpStatus = (int)response.StatusCode;
+                if (!utils.isOkResponse(httpStatus))
+                {
+                    if (retryTimes < Ais.RETRY_MAX_TIMES)
+                    {
+                        retryTimes++;
+                        Thread.Sleep(3000);
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                   
+                }
+                result = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                JObject joResult = (JObject)JsonConvert.DeserializeObject(result);
+                if (joResult["result"]["status"].ToString() == "failed")
+                {
+                    break;
+                } else if (joResult["result"]["status"].ToString() == "finish") {
+                    break;
+                }
+                else
+                {
+                    Thread.Sleep(3000);
+                    continue;
+                }
+
+            }
+            return result;
+
+        }
+
+        public static string ImageContentBatchJobsToken(string token, JArray urls, JArray categories, string endpoint)
+        {
+    
+            String jobId = GetImageContentJobId(token, urls, categories, endpoint);
+
+            return getImageContentResult(endpoint, jobId, token);
+        }
+
+
+        private static String GetImageContentJobId(String token, JArray urls, JArray categories, String endpoint)
+        {
+            // reuqest data for iamge content of batch jobs
+            JObject requestBody = new JObject();
+            requestBody.Add("categories", categories);
+            requestBody.Add("urls", urls);
+
+            HttpWebRequest request = null;
+            String result = null;
+            String uri = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.IMAGE_CONTENT_BATCH_JOBS).ToString();
+
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+            result = utils.PostData(request, uri, token, requestBody, result, serviceName);
+            JObject joResult = (JObject)JsonConvert.DeserializeObject(result);
+
+            return joResult["result"]["job_id"].ToString();
+        }
+
+        private static String getImageContentResult(String endpoint, String jobId, String token)
+        {
+            String result = null;
+            String url = new StringBuilder().Append("https://").Append(endpoint).Append(Ais.IMAGE_CONTENT_BATCH_RESULT).Append("?job_id=").Append(jobId).ToString();
+
+            HttpWebRequest request = null;
+            String serviceName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+            int retryTimes = 0;
+
+            while (true)
+            {
+                HttpWebResponse response = utils.getData(request, url, token, result, serviceName);
+                int httpStatus = (int)response.StatusCode;
+                if (!utils.isOkResponse(httpStatus))
+                {
+                    if (retryTimes < Ais.RETRY_MAX_TIMES)
+                    {
+                        retryTimes++;
+                        Thread.Sleep(3000);
+                        continue;
+                    }else
+                    {
+                        break;
+                    }
+                }
+                result = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                JObject joResult = (JObject)JsonConvert.DeserializeObject(result);
+                if (joResult["result"]["status"].ToString() == "failed")
+                {
+                    break;
+                }
+                else if (joResult["result"]["status"].ToString() == "finish") {
+                    break;
+                }
+                else { 
+                    Thread.Sleep(3000);
+                    continue;
+                }
+
+            }
+            return result;
+
+        }
+
+        public static String getEndponit(String region) {
+            String endpoint = endPointsDic[region];
+            return endpoint;
+        }
+    }
+}
