69f640790ecb62a51afe1e34cf65a46eff74d6cb
Meekol <942049649@qq.com>
2019-05-31 14:45:29 +0800

    go版本的内容审核服务sdk迁移

---
+++ go/sdk/utils.go
@@ -0,0 +1,66 @@
+package sdk
+
+import (
+	"encoding/base64"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+var ENDPOINTS = make(map[string]map[string]string)
+var region = "cn-north-1"
+
+func init(){
+	var moderationMap = make(map[string]string)
+	moderationMap["cn-north-1"] = "moderation.cn-north-1.myhuaweicloud.com"
+	moderationMap["ap-southeast-1"] = "moderation.ap-southeast-1.myhuaweicloud.com"
+	moderationMap["cn-north-4"] = "moderation.cn-north-4.myhuaweicloud.com"
+	ENDPOINTS["moderation"] = moderationMap
+}
+
+func ChangeFileToBase64(filepath string) string {
+	ff, _ := os.Open(filepath)
+	defer ff.Close()
+
+	imgByte, _ := ioutil.ReadAll(ff)
+	encodeString := base64.StdEncoding.EncodeToString(imgByte)
+	return encodeString
+}
+
+func DownFileByUrl(url string) string {
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err.Error())
+		return ""
+	}
+	defer res.Body.Close()
+
+	imgByte, _ := ioutil.ReadAll(res.Body)
+
+	encodeString := base64.StdEncoding.EncodeToString(imgByte)
+	return encodeString
+}
+
+func Base64ToFile(filePath string, base64Str string) {
+	buffer, _ := base64.StdEncoding.DecodeString(base64Str)
+	err := ioutil.WriteFile(filePath, buffer, 0666)
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func IsOkResponse(statusCode int) bool {
+	return statusCode >= 200 && statusCode <= 300
+}
+
+func InitRegion(regionName string){
+	region = regionName
+}
+
+func GetEndpoint(typeName string) string{
+	var endpointMap = make(map[string]string)
+	endpointMap = ENDPOINTS[typeName]
+	return endpointMap[region]
+}
