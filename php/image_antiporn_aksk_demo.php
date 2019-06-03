27b1635b071631793b67b7742da603bc7fd197c5
Meekol <942049649@qq.com>
2019-06-03 15:56:48 +0800

    php版本图像识别sdk增加cn-north-4的支持，修改引入的绝对路径，处理审核异步失败逻辑

--- php/image_antiporn_aksk_demo.php
+++ php/image_antiporn_aksk_demo.php
@@ -5,7 +5,7 @@
 require "./moderation_sdk/image_antiporn.php";
 require "./moderation_sdk/utils.php";
 
-// region目前支持华北-北京一(cn-north-1)、亚太-香港(ap-southeast-1)
+// region目前支持华北-北京一(cn-north-1)
 init_region($region = 'cn-north-1');
 
 $app_key = "*************";
