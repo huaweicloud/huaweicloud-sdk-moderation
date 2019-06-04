8800e6143f3e3153eefe20bd0f1b9d8ed6b6ad45
Meekol <942049649@qq.com>
2019-06-04 10:31:13 +0800

    内容审核扭曲矫正和清晰度检测服务注释更新

--- javascript/clarity_detect_aksk_demo.js
+++ javascript/clarity_detect_aksk_demo.js
@@ -4,7 +4,7 @@
 var clarity = require("./moderation_sdk/clarity_detect");
 var utils = require("./moderation_sdk/utils");
 
-// 初始化服务的区域信息，目前支持华北-北京(cn-north-1、cn-north-4)、亚太-香港(ap-southeast-1)等区域信息
+// 初始化服务的区域信息，目前支持华北-北京(cn-north-1)区域信息
 utils.initRegion("cn-north-1");
 
 var app_key = "*************";
