818d8533691f9781cc723623e336fdcf6e7c35e4
Meekol <942049649@qq.com>
2020-03-27 15:23:53 +0800

    补充文本及图像对上海一区域的支持

--- php/moderation_text_aksk_demo.php
+++ php/moderation_text_aksk_demo.php
@@ -5,7 +5,7 @@
 require "./moderation_sdk/moderation_text.php";
 require "./moderation_sdk/utils.php";
 
-// region目前支持华北-北京(cn-north-4)、亚太-香港(ap-southeast-1)
+// region目前支持华北-北京(cn-north-4)、华东上海一(cn-east-3)、亚太-香港(ap-southeast-1)
 init_region($region = 'cn-north-4');
 
 $app_key = "*********";
